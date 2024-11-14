6f96b297c4d1d00d3231f4fa6a021634a4df0af8
offro <offro@DESKTOP-998EDS4>
2024-11-14 15:14:29 +0900

    자재메인페이지작업중

--- GreenRePlat/src/main/java/com/team/green/material/service/MaterialService.java
+++ GreenRePlat/src/main/java/com/team/green/material/service/MaterialService.java
@@ -1,5 +1,24 @@
 package com.team.green.material.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.team.green.material.dao.IMaterialDAO;
+
 public class MaterialService {
+	
+@Service
+public class MaterialServie {
+	
+	@Autowired
+	IMaterialDAO dao;
+	/*
+	public List<MaterialDTO> getMaterialList(){
+		List<MaterialDTO> result = dao.getMaterialList();
+		return result;
+	}
+	*/
+}
+
 
 }
